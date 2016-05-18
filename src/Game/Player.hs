6bed78eb254ef222540f2038282c2381fbcfabca
Rick Winfrey <rewinfrey@github.com>
2016-05-17 22:36:39 -0500

    Update Game.Player to use String not Char

--- src/Game/Player.hs
+++ src/Game/Player.hs
@@ -1,13 +1,12 @@
 module Game.Player where
 
 data PlayerType  = Human | Computer deriving (Show, Eq)
-data Player      = Player { token :: Char, playerType :: PlayerType } deriving (Show, Eq)
+data Player      = Player { token :: String, playerType :: PlayerType } deriving (Show, Eq)
 
 newPlayerType :: String -> PlayerType
-newPlayerType playerType = case playerType of "human" -> Human
-                                              "computer" -> Computer
+newPlayerType playerType = case playerType of "Human" -> Human
+                                              "Computer" -> Computer
                                               _ -> Human
 
-newPlayer :: Char -> PlayerType -> Player
-newPlayer token playerType = Player { token = token, playerType = playerType }
-
+newPlayer :: String -> String -> Player
+newPlayer token playerType = Player { token = token, playerType = newPlayerType playerType }
