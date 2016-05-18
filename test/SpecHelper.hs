41a6de1f950c2c8c62954f8675a03dc3e1e7ae85
Rick Winfrey <rewinfrey@github.com>
2016-05-17 22:37:09 -0500

    Remove Data.String.Strip

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,13 +1,11 @@
 module SpecHelper
   ( module Test.Hspec
-  , module Data.String.Strip
   , module Game.Context
   , module Game.Player
   , module Game.Board
   ) where
 
 import Test.Hspec
-import Data.String.Strip
 import Game.Context
 import Game.Player
 import Game.Board
