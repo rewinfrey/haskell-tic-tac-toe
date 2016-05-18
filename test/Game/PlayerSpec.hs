cf9c6be8706e4e0626e71a40e7992837e98640bb
Rick Winfrey <rewinfrey@github.com>
2016-05-17 22:36:57 -0500

    Update per hlint

--- test/Game/PlayerSpec.hs
+++ test/Game/PlayerSpec.hs
@@ -7,23 +7,22 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "newPlayer" $ do
-    it "returns a new Player" $ do
-      let newPlayerType  = Human
-      let newPlayerToken = 'x'
-      let newPlayerTest  = newPlayer newPlayerToken newPlayerType
-      token newPlayerTest `shouldBe` newPlayerToken
-      playerType newPlayerTest `shouldBe` newPlayerType
+  describe "newPlayer" $
+    it "returns a new Player" $
+      do let newPlayerType  = "Human"
+         let newPlayerToken = "x"
+         let newPlayerTest  = newPlayer newPlayerToken newPlayerType
+         token newPlayerTest `shouldBe` newPlayerToken
+         playerType newPlayerTest `shouldBe` Human
 
-  describe "newPlayerType" $ do
-    it "returns a new PlayerType" $ do
-      let human    = "human"
-      let computer = "computer"
-      let unknown  = ""
-      let humanPlayer    = newPlayerType human
-      let computerPlayer = newPlayerType computer
-      let unknownPlayer  = newPlayerType unknown
-      humanPlayer `shouldBe` Human
-      computerPlayer `shouldBe` Computer
-      unknownPlayer `shouldBe` Human
-
+  describe "newPlayerType" $
+    it "returns a new PlayerType" $
+      do let human    = "Human"
+         let computer = "Computer"
+         let unknown  = ""
+         let humanPlayer    = newPlayerType human
+         let computerPlayer = newPlayerType computer
+         let unknownPlayer  = newPlayerType unknown
+         humanPlayer `shouldBe` Human
+         computerPlayer `shouldBe` Computer
+         unknownPlayer `shouldBe` Human
