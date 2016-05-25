3154666c9bed535f5ced178749ecf66ff6a435f8
Rick Winfrey <rewinfrey@github.com>
2016-05-25 14:58:10 -0600

    Add tie spec verifying a new board is not considered a tie

--- test/Game/BoardSpec.hs
+++ test/Game/BoardSpec.hs
@@ -73,9 +73,12 @@ spec = do
     it "returns false if the given board does not contain a win" $
       winner tieBoard `shouldBe` False
 
-  describe "tie" $
+  describe "tie" $ do
     it "returns true if the given board is a tie" $
       tie tieBoard `shouldBe` True
+
+    it "returns false if the given board is a blank board" $
+      tie (newBoard 3) `shouldBe` False
 
   describe "boardState" $ do
     it "returns Winner when winner is present" $
