e8597625769e97acc0b5c74787f8daf3931c1bb9
Rick Winfrey <rewinfrey@github.com>
2016-05-25 14:54:49 -0600

    Update Board spec for new Board type

--- test/Game/BoardSpec.hs
+++ test/Game/BoardSpec.hs
@@ -47,41 +47,8 @@ oppositeDiagonalWin =
                           (3,1) -> newSpace (3,1) X;     (3,2) -> newSpace (3,2) Blank; (3,3) -> newSpace (3,3) Blank
 spec :: Spec
 spec = do
+
   describe "newBoard" $ do
-    it "returns a new Board" $ do
-      newBoard 3 `shouldBe` (Board $ matrix 3 3 $ \(i,j) -> ' ')
-
-  describe "winner" $ do
-    it "returns true if the given board contains a column win" $ do
-      let winBoard = Board $ matrix 3 3 $ \(i,j) -> case (i,j) of (1,1) -> 'x'; (2,1) -> 'x'; (3,1) -> 'x'; (_,_) -> Game.Board.sentinel;
-      winner winBoard `shouldBe` True
-
-    it "returns true if the given board contains a row win" $ do
-      let winBoard = Board $ Data.Matrix.transpose $ matrix 3 3 $ \(i,j) -> case (i,j) of (1,1) -> 'x'; (1,2) -> 'x'; (1,3) -> 'x'; (_,_) -> Game.Board.sentinel;
-      winner winBoard `shouldBe` True
-
-    it "returns true if the given board contains a diagonal win" $ do
-      let winBoard = Board $ Data.Matrix.transpose $ matrix 3 3 $ \(i,j) -> case (i,j) of (1,1) -> 'x'; (2,2) -> 'x'; (3,3) -> 'x'; (_,_) -> Game.Board.sentinel;
-      winner winBoard `shouldBe` True
-
-    it "returns true if the given board contains an opposite diagonal win" $ do
-      let winBoard = Board $ Data.Matrix.transpose $ matrix 3 3 $ \(i,j) -> case (i,j) of (1,3) -> 'x'; (2,2) -> 'x'; (3,1) -> 'x'; (_,_) -> Game.Board.sentinel;
-      winner winBoard `shouldBe` True
-
-    it "returns false if the given board does not contain a win" $ do
-      let board = Board $ matrix 3 3 $ \(i,j) -> Game.Board.sentinel
-      winner board `shouldBe` False
-
-  describe "boardState" $ do
-    it "returns Winner when winner is present" $ do
-      let board = Board $ matrix 3 3 $ \(i,j) -> case i of 1 -> 'x'; 2 -> ' '; 3 -> Game.Board.sentinel
-      boardState board `shouldBe` Winner
-
-    it "returns Tie when tie is present" $ do
-      boardState tieBoard `shouldBe` Tie
-
-    it "returns Undecided when game is ongoing" $ do
-      boardState (newBoard 3) `shouldBe` Undecided
     it "returns a new Board with all Blank moves" $
       move <$> toList (newBoard 3) `shouldBe` Data.List.replicate 9 Blank
 
@@ -89,3 +56,29 @@ spec = do
       location <$> toList (newBoard 3) `shouldBe` [ (1,1), (1,2), (1,3)
                                                   , (2,1), (2,2), (2,3)
                                                   , (3,1), (3,2), (3,3)]
+
+  describe "winner" $ do
+    it "returns true if the given board contains a column win" $
+      winner columnWin `shouldBe` True
+
+    it "returns true if the given board contains a row win" $
+      winner rowWin `shouldBe` True
+
+    it "returns true if the given board contains a diagonal win" $
+      winner diagonalWin `shouldBe` True
+
+    it "returns true if the given board contains an opposite diagonal win" $
+      winner oppositeDiagonalWin `shouldBe` True
+
+    it "returns false if the given board does not contain a win" $
+      winner tieBoard `shouldBe` False
+
+  describe "boardState" $ do
+    it "returns Winner when winner is present" $
+      boardState columnWin `shouldBe` Winner
+
+    it "returns Tie when tie is present" $
+      boardState tieBoard `shouldBe` Tie
+
+    it "returns Undecided when game is ongoing" $
+      boardState (newBoard 3) `shouldBe` Undecided
