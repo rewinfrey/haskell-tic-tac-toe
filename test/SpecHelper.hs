d64ce385e3466b63d4784a667a1fe2ed58b5a4be
Rick Winfrey <rewinfrey@github.com>
2016-05-26 08:38:25 -0600

    Add helper functions for newSpace and newBlankBoard in SpecHelper

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -9,3 +9,9 @@ import Test.Hspec
 import Game.Context
 import Game.Player
 import Game.Board
+
+newSpace :: (Int, Int) -> Move -> Space
+newSpace location move = Space { location = location, move = move }
+
+newBlankBoard :: Board
+newBlankBoard = newBoard 3
