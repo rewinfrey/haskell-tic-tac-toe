ce28871f13851f9236e365ad0d11b187cca047d9
Rick Winfrey <rewinfrey@github.com>
2016-05-27 12:22:24 -0600

    First draft of minimax algorithm

--- src/Game/AIPlayer.hs
+++ src/Game/AIPlayer.hs
@@ -1,1 +1,37 @@
 module Game.AIPlayer where
+
+import Data.Foldable (minimumBy, maximumBy)
+import Game.Board( Board, Space(..), Move(X), Move(O), Space, Location, tempSpace, boardState, State( Winner) , State( Tie ), State( Undecided ), update, availableSpaces )
+
+-- Ply refers to the depth of the game tree as it is evaluated.
+type Ply = Int
+type Score = Int
+
+data MinMaxPlayer = Min | Max deriving (Eq, Show)
+
+minimax :: Board -> MinMaxPlayer -> Move -> Ply -> (Space, Score, MinMaxPlayer)
+minimax board minmaxPlayer move ply =
+  findBestMove minmaxPlayer $ evaluateBoard board minmaxPlayer move ply <$> availableSpaces board
+
+evaluateBoard :: Board -> MinMaxPlayer -> Move -> Ply -> Space -> (Space, Score, MinMaxPlayer)
+evaluateBoard board minmaxPlayer move ply space@ Space { location = location } =
+  let updatedBoard = update board location move in
+    case boardState updatedBoard of
+      Undecided -> minimax updatedBoard (oppositeMinMaxPlayer minmaxPlayer) (oppositeMove move) (ply + 1)
+      Winner -> (space { move = move }, score minmaxPlayer ply, minmaxPlayer)
+      Tie -> (space { move = move }, 0, minmaxPlayer)
+
+findBestMove :: MinMaxPlayer -> [(Space, Score, MinMaxPlayer)] -> (Space, Score, MinMaxPlayer)
+findBestMove minmaxPlayer scores = if minmaxPlayer == Max then maximumBy scoreComparison scores else minimumBy scoreComparison scores
+  where scoreComparison (_, score, minmax1) (_, score', minmax2) = if minmax1 /= minmax2
+                                                                    then compare score score'
+                                                                    else compare (abs score) (abs score')
+
+oppositeMove :: Move -> Move
+oppositeMove move = if move == X then O else X
+
+oppositeMinMaxPlayer :: MinMaxPlayer -> MinMaxPlayer
+oppositeMinMaxPlayer minmaxPlayer = if minmaxPlayer == Max then Min else Max
+
+score :: MinMaxPlayer -> Ply -> Score
+score minmaxPlayer ply = if minmaxPlayer == Max then 1000 - ply else (-1000) + ply
--- test/Game/AIPlayerSpec.hs
+++ test/Game/AIPlayerSpec.hs
@@ -7,8 +7,65 @@ import SpecHelper
 main :: IO ()
 main = hspec spec
 
+columnWinOpportunity :: Board
+columnWinOpportunity =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) X; (1,2)     -> newSpace (1,2) Blank; (1,3) -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) X; (2,2)     -> newSpace (2,2) Blank; (2,3) -> newSpace (2,3) Blank
+                          (3,1) -> newSpace (3,1) Blank; (3,2) -> newSpace (3,2) Blank; (3,3) -> newSpace (3,3) Blank
+
+rowWinOpportunity :: Board
+rowWinOpportunity =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) X; (1,2)     -> newSpace (1,2) X; (1,3)     -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) X; (2,2)     -> newSpace (2,2) O; (2,3)     -> newSpace (2,3) Blank
+                          (3,1) -> newSpace (3,1) O; (3,2)     -> newSpace (3,2) Blank; (3,3) -> newSpace (3,3) Blank
+
+winAndOpponentWinOpportunity :: Board
+winAndOpponentWinOpportunity =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) X; (1,2)     -> newSpace (1,2) X; (1,3)     -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) X; (2,2)     -> newSpace (2,2) O; (2,3)     -> newSpace (2,3) Blank
+                          (3,1) -> newSpace (3,1) O; (3,2)     -> newSpace (3,2) O; (3,3)     -> newSpace (3,3) Blank
+
+blockOpponentWinOpportunity :: Board
+blockOpponentWinOpportunity =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) X; (1,2)     -> newSpace (1,2) Blank; (1,3) -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) X; (2,2)     -> newSpace (2,2) Blank; (2,3) -> newSpace (2,3) Blank
+                          (3,1) -> newSpace (3,1) O; (3,2)     -> newSpace (3,2) O; (3,3)     -> newSpace (3,3) Blank
+
+blockOpponentForkOpportunity :: Board
+blockOpponentForkOpportunity =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) O; (1,2)     -> newSpace (1,2) Blank; (1,3) -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) Blank; (2,2) -> newSpace (2,2) Blank; (2,3) -> newSpace (2,3) Blank
+                          (3,1) -> newSpace (3,1) Blank; (3,2) -> newSpace (3,2) Blank; (3,3) -> newSpace (3,3) Blank
+
+blockOpponentForkOpportunity2 :: Board
+blockOpponentForkOpportunity2 =
+  matrix 3 3 $ \(i,j) -> case (i,j) of
+                          (1,1) -> newSpace (1,1) O;     (1,2) -> newSpace (1,2) X;     (1,3) -> newSpace (1,3) Blank
+                          (2,1) -> newSpace (2,1) X;     (2,2) -> newSpace (2,2) Blank; (2,3) -> newSpace (2,3) O
+                          (3,1) -> newSpace (3,1) Blank; (3,2) -> newSpace (3,2) O;     (3,3) -> newSpace (3,3) Blank
+
 spec :: Spec
 spec =
-  describe "minimax" $
-    it "returns the best move" $
-      True `shouldBe` True
+  describe "minimax" $ do
+    it "finds immediate win simple case" $
+      minimax columnWinOpportunity Max X 0 `shouldBe` (Space { location = (3,1), move = X }, 1000, Max)
+
+    it "finds immediate win more complex case" $
+      minimax rowWinOpportunity Max X 0 `shouldBe` (Space { location = (1,3), move = X }, 1000, Max)
+
+    it "prioritizes wins over blocking opponent's win" $
+      minimax winAndOpponentWinOpportunity Max X 0 `shouldBe` (Space { location = (1,3), move = X }, 1000, Max)
+
+    it "blocks an opponent from winning" $
+      minimax blockOpponentWinOpportunity Max X 0 `shouldBe` (Space { location = (3,3), move = O }, -999, Min)
+
+    it "blocks an opponent from forking" $
+      minimax blockOpponentForkOpportunity Max X 0 `shouldBe` (Space { location = (1,3), move = O }, -995, Min)
+
+    it "blocks an opponent from forking two" $
+      minimax blockOpponentForkOpportunity2 Max X 0 `shouldBe` (Space { location = (3,3), move = O }, -997, Min)
