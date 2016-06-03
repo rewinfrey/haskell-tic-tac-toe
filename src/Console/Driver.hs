dad2bdf825ed3dbfd4e393bb788f5eb2ff296ef8
Rick Winfrey <rewinfrey@github.com>
2016-06-02 23:33:47 -0500

    Update Console.Driver to play basic game

--- src/Console/Driver.hs
+++ src/Console/Driver.hs
@@ -1,9 +1,10 @@
 module Console.Driver where
 
 import System.Process
+import Game.AIPlayer ( minimax, MinMaxPlayer(..) )
+import Game.Board ( update, boardState, State(..), Board, Space(..), Location )
 import Game.Context
-import Game.Player
-import Game.Board ( Board, Move(..), Space(..) )
+import Game.Player ( Token(..), newPlayer, Player(..), PlayerType(..) )
 import Data.Foldable (maximumBy)
 import Data.List (intercalate)
 import Data.Matrix (toLists)
@@ -11,7 +12,7 @@ import Data.Matrix (toLists)
 x = "X"
 o = "O"
 
-defaultBoardSize = 3
+defaultBoardSize = 4
 
 human = "Human"
 computer = "Computer"
@@ -19,28 +20,8 @@ computer = "Computer"
 start :: IO ()
 start = do
   greeting
-  player1TokenSelection <- getToken
-  player1TypeSelection  <- getPlayer "1"
-  player2TypeSelection  <- getPlayer "2"
-
-  let player1Token = inputFromChoice player1TokenSelection x o
-  let player1Type  = inputFromChoice player1TypeSelection human computer
-  let player2Type  = inputFromChoice player2TypeSelection human computer
-
-  putStrLn $ "\nPlayer 1's token is: " ++ player1Token
-  putStrLn $ "\nPlayer 1 is: " ++ player1Type
-  putStrLn $ "\nPlayer 2 is: " ++ player2Type
-
-  let player1 = newPlayer player1Token player1Type
-  let player2 = newPlayer (opposite player1Token) player2Type
-
-  let gameContext = newGameContext player1 player2 defaultBoardSize
-
-  putStrLn $ "\nGameContext: " ++ show gameContext
-
-  putStrLn $ "\nPlayer 1 real token is: " ++ token player1
-  putStrLn $ "\nPlayer 2 real token is: " ++ token player2
-  putStrLn "\nFinished!"
+  gameContext <- setupGame
+  playGame gameContext
 
 opposite :: String -> String
 opposite x = o
@@ -48,6 +29,73 @@ opposite o = x
 
 greeting :: IO ()
 greeting = putStrLn "\nWelcome to Haskell Tic-Tac-Toe!\n"
+
+setupGame :: IO GameContext
+setupGame = do
+  player1TokenSelection <- getToken
+  player1TypeSelection  <- getPlayer "1"
+  player2TypeSelection  <- getPlayer "2"
+
+  player1Token <- inputFromChoice player1TokenSelection x o
+  player1Type  <- inputFromChoice player1TypeSelection human computer
+  player2Type  <- inputFromChoice player2TypeSelection human computer
+
+  let player1 = newPlayer player1Token player1Type
+  let player2 = newPlayer (opposite player1Token) player2Type
+
+  return $ newGameContext player1 player2 defaultBoardSize
+
+playGame :: GameContext -> IO ()
+playGame gameContext@ GameContext { board = board, player1 = player1, player2 = player2, currentPlayer = currentPlayer } = do
+  displayBoard board
+  move <- if playerType currentPlayer == Human then humanPlayerTurn else computerPlayerTurn currentPlayer board
+  let updatedBoard = update board move (Game.Player.token currentPlayer)
+  let nextPlayer = if currentPlayer == player1 then player2 else player1
+  case boardState updatedBoard of
+    Winner    -> displayWinner updatedBoard currentPlayer
+    Tie       -> displayTie updatedBoard
+    Undecided -> playGame (gameContext { board = updatedBoard, currentPlayer = nextPlayer })
+
+displayWinner :: Board -> Player -> IO ()
+displayWinner board currentPlayer = do
+  displayBoard board
+  putStrLn $ show (Game.Player.token currentPlayer) ++ " is the winner!"
+
+displayTie :: Board -> IO ()
+displayTie board = do
+  displayBoard board
+  putStrLn "It's a tie!"
+
+humanPlayerTurn :: IO Location
+humanPlayerTurn = do
+    humanMovePrompt
+    move <- getLine
+    return (read move :: Location)
+
+computerPlayerTurn :: Player -> Board -> IO Location
+computerPlayerTurn player board = do
+  computerMovePrompt
+  return (minimax board (token player))
+
+humanMovePrompt :: IO ()
+humanMovePrompt =
+  putStrLn "Please choose a move location (e.g. (1,1))"
+
+computerMovePrompt :: IO ()
+computerMovePrompt =
+  putStrLn "Computer is thinking..."
+
+getToken :: IO String
+getToken = do
+  tokenPrompt
+  choice <- getLine
+  if inputValid choice then return choice else getToken
+
+getPlayer :: String -> IO String
+getPlayer playerNumber = do
+  playerPrompt playerNumber
+  choice <- getLine
+  if inputValid choice then return choice else getPlayer playerNumber
 
 tokenPrompt :: IO ()
 tokenPrompt = do
@@ -64,18 +112,6 @@ playerPrompt playerNumber = do
 invalidInputPrompt :: IO ()
 invalidInputPrompt = putStrLn "\nSorry, I dunno that!"
 
-getToken :: IO String
-getToken = do
-  tokenPrompt
-  choice <- getLine
-  if inputValid choice then return choice else getToken
-
-getPlayer :: String -> IO String
-getPlayer playerNumber = do
-  playerPrompt playerNumber
-  choice <- getLine
-  if inputValid choice then return choice else getPlayer playerNumber
-
 inputValid :: String -> Bool
 inputValid choice =
   case choice of
@@ -83,11 +119,11 @@ inputValid choice =
     "2" -> True
     _   -> False
 
-inputFromChoice :: String -> String -> String -> String
+inputFromChoice :: String -> String -> String -> IO String
 inputFromChoice choice opt1 opt2 =
   case choice of
-    "1" -> opt1
-    "2" -> opt2
+    "1" -> return opt1
+    "2" -> return opt2
 
 displayBoard :: Board -> IO ()
 displayBoard board = do
@@ -99,7 +135,7 @@ displayBoard board = do
 
 rowToString :: [Space] -> String
 rowToString spaces = Data.List.intercalate " | " $ spaceToString <$> spaces
-  where spaceToString Space { location = location, move = move } = case move of
-                                                                     Blank -> show location
-                                                                     X -> "  X  "
-                                                                     O -> "  O  "
+  where spaceToString Space { location = location, spaceToken = spaceToken } = case spaceToken of
+                                                                             Blank -> show location
+                                                                             X -> "  X  "
+                                                                             O -> "  O  "
